68567aa6fbe9666f256b6023b64470622d2aca50
Jennifer Maia <jen_maia@hotmail.com>
2025-07-18 18:05:31 -0300

    Criando a função de load e a pipeline.py

--- etl.py
+++ etl.py
@@ -17,11 +17,31 @@ def calcular_kpi_de_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
     df["Total"] = df["Quantidade"] * df["Venda"]
     return df
 # Uma função que da load em csv ou parquet
+#Essa função é uma procedure porque não tem um return
 
+
+def carregar_dados(df: pd.DataFrame, format_saida: list):
+    """
+    parametro que vai ser ou "csv" ou "parquet" ou "os dois"
+    """
+    for formato in format_saida:
+        if formato == 'csv':
+            df.to_csv("dados.csv", index=False)
+        if formato == 'parquet':
+            df.to_parquet("dados.parquet", index=False)
+
+# Função que vai chamar tudo!
+
+def pipeline_calcular_kpi_de_vendas_consolidado(pasta: str, formato_de_saida: list):
+    data_frame = extrair_dados_e_consolidar(pasta)
+    data_frame_calculado = calcular_kpi_de_total_de_vendas(data_frame)
+    carregar_dados(data_frame_calculado, formato_de_saida)
 
 ## Para testar os desenvolvimentos: (Assim não fica nenhum "lixo" no codigo principal)
 
-if __name__ == "__main__":
-    pasta_argumento = 'data'
-    data_frame = extrair_dados_e_consolidar(pasta=pasta_argumento)
-    print(calcular_kpi_de_total_de_vendas(data_frame))
+# if __name__ == "__main__":
+#     pasta_argumento: str = 'data'
+#     data_frame = extrair_dados_e_consolidar(pasta=pasta_argumento)
+#     data_frame_calculado = calcular_kpi_de_total_de_vendas(data_frame)
+#     formato_de_saida: list = ["csv", "parquet"]
+#     carregar_dados(data_frame_calculado, formato_de_saida)
